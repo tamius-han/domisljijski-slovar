af7a6d14f595e0590ccfe87b2e2ea59d244757cf
Tamius Han <ziga.pirih@hotmail.com>
2021-03-14 15:26:02 +0100

    Add vue router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import vueDebounce from 'vue-debounce';
 import App from './App.vue';
+import router from './router'
 
 Vue.config.productionTip = false
 
 Vue.use(vueDebounce);
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
