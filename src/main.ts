f3b18fe0e45d997d9151b51d58c9e2024511e277
Tamius Han <ziga.pirih@hotmail.com>
2021-03-14 16:30:41 +0100

    add google 2fa thing to app

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import vueDebounce from 'vue-debounce';
-
+import GAuth from 'vue3-google-oauth2';
+import env from '../config/env';
 
 createApp(App)
   .use(router)
   .use(vueDebounce)
+  .use(GAuth, {
+    clientId: env.GOOGLE_CLIENT_ID,
+    scope: 'email',
+    fetchBasicProfile: false
+  })
   .mount('#app');
