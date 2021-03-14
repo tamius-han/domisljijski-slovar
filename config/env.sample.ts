205fe0525b7475411d02c5a0699774138fcd2c9d
Tamius Han <ziga.pirih@hotmail.com>
2021-03-14 16:30:11 +0100

    sample env file

---
+++ config/env.sample.ts
@@ -0,0 +1,11 @@
+// This file contains all the .env fields the
+// project uses and serves purely as a non-working
+// sample.
+
+const env = {
+  GOOGLE_CLIENT_ID: 'id',
+  DICTIONARY_API_BASE: 'url',
+  ADMIN_API_BASE: 'url'
+};
+
+export default env;
