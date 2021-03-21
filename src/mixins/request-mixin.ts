f750924fbb83b440e7684d77a1b321dc98c7ea57
Tamius Han <ziga.pirih@hotmail.com>
2021-03-21 01:04:49 +0100

    Have axios requests in a separate file

---
+++ src/mixins/request-mixin.ts
@@ -0,0 +1,48 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import env from '../../config/env';
+
+const requestMixin = {
+  methods: {
+    setAuthToken(token: string): void {
+      window.localStorage.setItem('userToken', token);
+    },
+    getAuthToken(): string | null {
+      return window.localStorage.getItem('userToken');
+    },
+    async get(url: string, options?: AxiosRequestConfig) {
+      if (! (url.startsWith('http://') || url.startsWith('https://'))) {
+        url = `${env.ADMIN_API_BASE}${url}`;
+      }
+
+      return axios.get(
+        url,
+        {
+          ...options,
+          headers: {
+            ...options?.headers,
+            'Authorization': this.getAuthToken()
+          }
+        }
+      )
+    },
+    async post(url: string, data?: any, options?: AxiosRequestConfig) {
+      if (! (url.startsWith('http://') || url.startsWith('https://'))) {
+        url = `${env.ADMIN_API_BASE}${url}`;
+      }
+
+      return axios.post(
+        url,
+        data,
+        {
+          ...options,
+          headers: {
+            ...options?.headers,
+            'Authorization': this.getAuthToken()
+          }
+        }
+      )
+    }
+  }
+};
+
+export default requestMixin;
