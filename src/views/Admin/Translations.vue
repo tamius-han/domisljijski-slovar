ac1fce5094a6b743f86d1b4fdd8581644d987088
Tamius Han <ziga.pirih@hotmail.com>
2021-03-21 01:05:50 +0100

    Do most of the work on the translations page

--- src/views/Admin/Translations.vue
+++ src/views/Admin/Translations.vue
@@ -1,24 +1,61 @@
 <template>
   <div class="root">
     <div class="flex flex-column">
-      <div>
-        add [word] &lt;=&gt; [word] thing here
+
+      <div class="page">
+        <div class="content">
+          <div class="translation-box">
+            <div class="translation-heading">
+              PREDLAGAN PREVOD
+            </div>
+            <div class="translation-proposed flex flex-row">
+              <div class="word word-left">{{enWord?.word ?? '<angleški izraz>'}} :</div><div class="word word-right">: {{slWord?.word ?? '<slovenski izraz>'}}</div>
+            </div>
+            <div class="field">
+              <div class="field-label">
+                Prioriteta prevoda
+              </div>
+              <div class="input">
+                <input>
+              </div>
+            </div>
+            <div class="field">
+              <div class="field-label">
+                Opombe prevoda
+              </div>
+              <div class="input-textarea">
+                <textarea></textarea>
+              </div>
+            </div>
+            <div class="field">
+              <div class="field-label">
+                <input type="checkbox"> Na ravni zamisli
+              </div>
+            </div>
+            <div class="button-wrapper">
+              <div class="button" @click="addTranslation()">Dodaj prevod</div>
+            </div>
+          </div>
+        </div>
       </div>
 
-
-      <div class="flex flex-row">
+      <div class="flex flex-row word-selector-container">
         <word-selector
           language="Angleščina"
           languageKey="en"
-        ></word-selector>    
+          @change="setEnglishWord($event)"
+        ></word-selector>
+        <div class="spacer"></div>
         <word-selector
           language="Slovenščina"
           languageKey="sl"
+          @change="setSlovenianWord($event)"
         ></word-selector>    
       </div>
     </div>
 
-    <div class="word-slovenian">
+    <div class="">
+      <a @click="logout()">logout — test</a>
     </div>
   </div>
 </template>
@@ -26,15 +63,89 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import WordSelector from '../../components/WordSelector.vue';
+import requestMixin from '../../mixins/request-mixin';
 
 export default defineComponent({
   name: 'Translations',
   components: {
     WordSelector
+  },
+  mixins: [
+    requestMixin
+  ],
+  data() {
+    return {
+      enWord: undefined as any,
+      slWord: undefined as any
+    }
+  },
+  methods: {
+    setEnglishWord(word: any) {
+      console.info('change:', word);
+      this.enWord = word;
+    },
+    setSlovenianWord(word: any) {
+      console.info('change:', word);
+      this.slWord = word;
+    },
+    async addTranslation() {
+      if (!this.enWord || !this.slWord) {
+        return;
+      }
+      await this.post(
+        `/translations/`,
+        {
+          enWordId: this.enWord?.id,
+          slWordId: this.slWord?.id
+        }
+      );
+    },
+
+    logout() {
+
+    }
   }
 })
 </script>
 
 <style lang="scss" scoped>
+$primary-text: rgb(172, 103, 48);
 
+.translation-box {
+  margin-top: 2rem;
+  text-align: center;
+
+  .translation-heading {
+    font-size: 0.85rem;
+  }
+}
+
+.translation-proposed {
+  box-sizing: border-box;
+
+  font-family: 'Vollkorn';
+
+  width: 100%;
+
+  .word {
+    box-sizing: border-box;
+    width: 50%;
+    color: $primary-text;
+    font-size: 2rem !important;
+  }
+  .word-left {
+    text-align: right;
+  }
+  .word-right {
+    text-align: left;
+  }
+}
+
+.word-selector-container {
+  justify-content: center;
+
+  .spacer {
+    width: 4.2rem;
+  }
+}
 </style>
