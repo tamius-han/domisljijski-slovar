7b7dc83bf9ff4a5476ad4caa2c03ef7a1c050cd8
Tamius Han <ziga.pirih@hotmail.com>
2022-06-01 01:26:39 +0200

    add categories selector to migrations page (+ other updates)

--- src/views/Migration.vue
+++ src/views/Migration.vue
@@ -6,13 +6,14 @@
 
 Alternativno lahko pripopaš sem notri export (json string)<br/>
 <textarea v-model="processedDataJson"></textarea><br/>
-<a @click="getTermsFromJson()">Uvozi besede, ampak iz te besediloškatle</a> ** <a @click="processedDataJson = JSON.stringify(processedData, null, 2)">dej mi te besede nazaj v iščiškatlo</a>
+<a @click="getTermsFromJson()">Uvozi besede, ampak iz te besediloškatle</a> ** <a @click="mergeTermsFromJson()">združi prikazano z Json Bourne</a>  ** <a @click="processedDataJson = JSON.stringify(processedData, null, 2)">dej mi te besede nazaj v iščiškatlo</a>
+<br/><br/>
+<a @click="processedData.sort((a,b)=>a.enWord.word?.localeCompare(b.enWord.word))">sort alphabetically <b>BUT ONLY AFTER YOU'VE FINISHED DOING CREDITS, TYPES, AND CATEGORIES</b></a>
 
 <div v-for="(word, index) in processedData" :key="index" class="fr">
-
   <!-- ENGLISH -->
   <div class="fc" style="border: 1px solid #000; background-color: rgba(255,128,64, 0.25); margin: 0.25rem;">
-    <div>EN WORD</div>
+    <div class="fr fw"><b>{{index}}</b> :: EN WORD — <small> id override: <input class="override" v-model="word.enWord.id" /></small></div>
     <div class="fr">
       <div class="fc">
         Word
@@ -35,11 +36,12 @@ Alternativno lahko pripopaš sem notri export (json string)<br/>
 
   <!-- ENGLISH - MEANING -->
   <div class="fc" style="border: 1px solid #000; background-color: rgba(255,128,64, 0.5); margin: 0.25rem;">
-    <div>EN MEANING</div> <small>— {{word.enMeaning.meaning}}</small>
+    <div class="fr fw">EN MEANING <small> — id override: <input class="override" v-model="word.enMeaning.id" /></small></div>
+    <small>— {{word.enMeaning.meaning}} </small>
     <div class="fr">
       <div class="fc">
         MEANING
-        <input v-model="word.enMeaning.meaning"/>
+        <textarea v-model="word.enMeaning.meaning"></textarea>
       </div>
       <div class="fc">
         Type (1 n, 2 v, 3 adj)
@@ -58,15 +60,16 @@ Alternativno lahko pripopaš sem notri export (json string)<br/>
 
   <!-- SLO - MEANING -->
   <div class="fc" style="border: 1px solid #000; background-color: rgba(128,128,255, 0.5); margin: 0.25rem;">
-    <div>SLO MEANING</div> <small>— {{word.slMeaning.meaning}}</small>
+    <div class="fr fw">SLO MEANING <small> — id override: <input class="override" v-model="word.slMeaning.id" /></small></div>
+    <small>— {{word.slMeaning.meaning}} </small>
     <div class="fr">
       <div class="fc">
         MEANING
-        <input v-model="word.slMeaning.meaning"/>
+        <textarea v-model="word.slMeaning.meaning"></textarea>
       </div>
       <div class="fc">
         notes
-        <input v-model="word.slMeaning.notes"/>
+        <textarea v-model="word.slMeaning.notes"></textarea>
       </div>
       <div class="fc">
         meaning prio (EN ← SI)
@@ -89,7 +92,7 @@ Alternativno lahko pripopaš sem notri export (json string)<br/>
       </div>
       <div class="fc">
         Notes
-        <input v-model="word.slWord.notes"/>
+        <textarea v-model="word.slWord.notes"></textarea>
       </div>
       <div class="fc">
         credit
@@ -106,21 +109,34 @@ Alternativno lahko pripopaš sem notri export (json string)<br/>
     </div>
   </div>
 
-  <div class="fc" style="border: 1px solid #000; background-color: rgba(255,128,64, 0.5); margin: 0.25rem;">
-    <div>CATEGORIES</div>
-    <div class="fr">
-      <div class="fc">
-        (comma separated)
-        <input v-model="word.categories" @input="updateCategories(index)"/>
-      </div>
-    </div>
-  </div>
-
   <div class="fc">
     <a @click="word.skip = !word.skip">CLICK TO SKIP</a>
     <div v-if="word.skip"><b>(this word won't be added)</b></div>
     <div v-else>word ok</div>
   </div>
+
+  <div class="fc rel ch" style="border: 1px solid #000; background-color: rgba(255,128,64, 0.5); margin: 0.25rem;">
+    <div>CATEGORIES</div>
+    <div class="fr">
+      <div class="fc">
+        (comma separated)
+        <input :value="word.categories.join(',')" @input="updateCategories($event, index)"/>
+      </div>
+    </div>
+
+    <div class="category-select">
+      <CategoryTree
+        :categories="categories"
+        :value="word.categories"
+        @input="updateCategories($event, index)"
+        :depth="0"
+        :selectParents="true"
+        languagePriority="sl"
+      >
+      </CategoryTree>
+    </div>
+  </div>
+
 
 </div>
 
@@ -134,6 +150,7 @@ Alternativno lahko pripopaš sem notri export (json string)<br/>
 <script>
 import { defineComponent } from 'vue';
 import requestMixin from '@/mixins/request-mixin';
+import CategoryTree from '../components/CategoryTree.vue';
 
 export default defineComponent({
   data() {
@@ -141,14 +158,20 @@ export default defineComponent({
       v1_api: '',
       processedData: [],
       forceReloadPls: true,
-      processedDataJson: ''
+      processedDataJson: '',
+      categories: [] ,
+      selectedCategories: [],
     }
   },
   mixins: [
     requestMixin
   ],
-  computed: {
-
+  components: {
+    CategoryTree,
+  },
+  async mounted() {
+    const categories = await this.getCategories();
+    this.categories = categories;
   },
   methods: {
     updateWordType(index) {
@@ -161,8 +184,13 @@ export default defineComponent({
         this.processedData[i].enWord.type = this.processedData[index].enWord.type;
       }
     },
-    updateCategories(index) {
+    updateCategories(value, index) {
+      console.log('updating categories. value:', value, 'index:', index);
+      if (!Array.isArray(value)) {
+        value = value.split(',').map(x => +x);
+      }
       // we also bank that similar categories are local to each other
+      this.processedData[index].categories = value;
       for (let i = index + 1; i < this.processedData.length; i++) {
         this.processedData[i].categories = this.processedData[index].categories;
       }
@@ -175,6 +203,14 @@ export default defineComponent({
     },
     getTermsFromJson() {
       this.processedData = JSON.parse(this.processedDataJson)
+    },
+    mergeTermsFromJson() {
+      const processedJson = JSON.parse(this.processedDataJson);
+
+      for (const entry of processedJson) {
+        const i = this.processedData.findIndex(x => x.enWord.word === entry.enWord.word && x.enMeaning.meaning === entry.enMeaning.meaning && x.slWord.word === entry.slWord.word);
+        this.processedData[i] = {...this.processedData[i], ...entry};
+      }
     },
     async getTerms() {
       const data = [];
@@ -212,7 +248,7 @@ export default defineComponent({
             meaningPriority: 0,
             type: 0,
           };
-          const categories = '';
+          const categories = [];
           const slWord = {
             language: 'sl',
             word: word.sl_word,
@@ -222,6 +258,7 @@ export default defineComponent({
           }
 
           data.push({
+            source_enId: +word.en_id,
             enWord,
             enMeaning,
             slMeaning,
@@ -232,7 +269,7 @@ export default defineComponent({
         }
       }
 
-      data.sort((a, b) => a.enWord.word?.localeCompare(b.enWord.word));
+      data.sort((a, b) => a.sourceId - b.sourceId);
       this.processedData = data;
     },
     getCategoriesFromTree(categories, allCategories) {
@@ -410,14 +447,43 @@ export default defineComponent({
 
 </script>
 
-<style>
+<style lang="scss">
 .fr {
   display: flex;
   flex-direction: row;
   flex-wrap: nowrap;
 }
+.fw {
+  flex-wrap: wrap !important;
+}
 .fc {
   display: flex;
   flex-direction: column;
+}
+.override {
+  background-color: transparent;
+  border: 0px solid transparent;
+  border-bottom: 1px dashed #666;
+  outline: none;
+}
+
+.rel {
+  position: relative;
+}
+
+.category-select {
+  position: absolute;
+  right: 0;
+  top: 100%;
+  background-color: #fff;
+  width: 16rem;
+  display: none;
+  z-index: 1000;
+}
+
+.ch:hover {
+  .category-select {
+    display: block !important;
+  }
 }
 </style>
